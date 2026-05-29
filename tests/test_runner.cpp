54d301f02e95d19e8fbf907923e9fdcb4f0e8a80
DrChrisHax <cmanlove1234@outlook.com>
2026-05-29 12:01:52 -0700

    Added time to the test suite

--- tests/test_runner.cpp
+++ tests/test_runner.cpp
@@ -3,20 +3,30 @@
 #include "sample_tests.hpp"
 // Add other test headers above this line
 
+#include "timer.hpp"
+
 #include <iostream>
 #include <iomanip>
 #include <string_view>
 
+using core::Timer;
+
 static int total_tests = 0;
 static int passed_tests = 0;
 static int failed_tests = 0;
+
+static Timer::Duration total_test_time_ns{};
 
 void Run(test_result (*test)(), std::string_view name) {
     ++total_tests;
 
     std::cout << CYAN << "[TEST]" << RESET << ' ' << name << '\n';
 
+    Timer timer;
+    timer.start();
     test_result result = test();
+    timer.stop();
+    total_test_time_ns += timer.elapsed();
 
     if (result.passed()) {
         ++passed_tests;
@@ -27,7 +37,7 @@ void Run(test_result (*test)(), std::string_view name) {
         std::cout << RED << "[FAIL] Result:   " << RESET << result.result() << '\n';
     }
 
-    std::cout << std::endl; // Flush the buffer between tests
+    std::cout << CYAN << "[TIME] " << RESET << timer.elapsed_str() << '\n' << std::endl; // Flush the buffer between tests
 }
 
 int main(int argc, char* argv[]) {
@@ -35,10 +45,12 @@ int main(int argc, char* argv[]) {
     (void)argc;
     (void)argv;
 
-    std::cout << "========================================" << std::endl;
-    std::cout << "Running Test Suite" << std::endl;
-    std::cout << "========================================" << std::endl;
-    std::cout << std::endl;
+    Timer timer;
+    timer.start();
+
+    std::cout << "========================================\n";
+    std::cout << "Running Test Suite\n";
+    std::cout << "========================================\n\n";
 
     // =============================================================================
     // Sample Tests
@@ -47,22 +59,30 @@ int main(int argc, char* argv[]) {
     Run(SampleTest1, "Sample Test 1"); // This test will pass
     Run(SampleTest2, "Sample Test 2"); // This test will fail
 
-    std::cout << std::endl;
-
     // =============================================================================
     // Future Tests
     // =============================================================================
 
-    //std::cout << std::endl;
-      
     // =============================================================================
     // Test Summary
     // =============================================================================
-    std::cout << "========================================" << std::endl;
-    std::cout << "Test Summary" << std::endl;
-    std::cout << "========================================" << std::endl;
-    std::cout << "Total: " << total_tests << std::endl;
-    std::cout << "Passed: " << passed_tests << std::endl;
-    std::cout << "Failed: " << failed_tests << std::endl;
-    std::cout << std::endl;
+     
+    std::cout << "========================================\n";
+    std::cout << "Test Summary\n";
+    std::cout << "========================================\n";
+    std::cout << CYAN  << "[TOTAL]  " << RESET << total_tests << '\n';
+    std::cout << GREEN << "[PASSED] " << RESET << passed_tests << '\n';
+    std::cout << RED   << "[FAILED] " << RESET << failed_tests << '\n';
+
+    // Time 1: sum of individual test times
+    // Time 2: total test suite run time
+    timer.stop();
+
+    std::string time_1 = timer.elapsed_str(total_test_time_ns);
+    std::string time_2 = timer.elapsed_str();
+
+    std::cout << "Individual tests time sum: " << time_1 << '\n';
+    std::cout << "Total test suite run time: " << time_2 << std::endl;
+
+    return 0;
 }
